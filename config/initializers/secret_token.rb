58f0a03ac644147c8768857cf920debc23fac74d
Flo Schmitz <fschmitz813@gmail.com>
2013-07-10 13:44:18 -0500

    Icreated a rails app and added scaffolding for statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'a9045c0ba2aa85a17aa34186635bb661164778da713aadafe3870975d15e7424389224c859917ba43605c3ababb86fab3e9b1e87765e37ad3903dc4bad11a971'
